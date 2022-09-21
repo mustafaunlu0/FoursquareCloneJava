cda9a7f7713b459512e0674533f5e76d730d0f6b
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-21 17:25:32 +0300

    menu is added to the MainActivity and the connection is established between activities

--- app/src/main/java/com/mustafaunlu/foursquareclone/activity/MainActivity.java
+++ app/src/main/java/com/mustafaunlu/foursquareclone/activity/MainActivity.java
@@ -1,9 +1,20 @@
 package com.mustafaunlu.foursquareclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.mustafaunlu.foursquareclone.R;
 import com.mustafaunlu.foursquareclone.databinding.ActivityMainBinding;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +25,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.createPlace){
+            //to DetailActivity
+        }else if(item.getItemId() == R.id.logOut){
+            //log out
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if( e != null){
+                        Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }else{
+                        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/mustafaunlu/foursquareclone/activity/PlaceActivity.java
@@ -0,0 +1,16 @@
+package com.mustafaunlu.foursquareclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mustafaunlu.foursquareclone.R;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+}
