aeea4ca2f13a279d0b858f314db7cfcbf60391ca
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-22 21:41:30 +0300

    maps activity error!

--- app/src/main/java/com/mustafaunlu/foursquareclone/activity/MapsActivity.java
+++ app/src/main/java/com/mustafaunlu/foursquareclone/activity/MapsActivity.java
@@ -11,8 +11,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -31,15 +37,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.snackbar.Snackbar;
 import com.mustafaunlu.foursquareclone.R;
 import com.mustafaunlu.foursquareclone.databinding.ActivityMapsBinding;
+import com.mustafaunlu.foursquareclone.model.Place;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.io.ByteArrayOutputStream;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-    private GoogleMap getmMap;
+
+    //LocationManager & LocationListener
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     //Launcher
     private ActivityResultLauncher<String> permissionLauncher;
+
+    //Selected Latitude & Longitude
+    private String selectedLatitudeString;
+    private String selectedLongitudeString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +81,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        getmMap=googleMap;
+        //mMap=googleMap;
+        //mMap.setOnMapLongClickListener(this);
 
 
 
@@ -84,6 +106,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        selectedLatitudeString=String.valueOf(latLng.latitude);
+        selectedLongitudeString=String.valueOf(latLng.longitude);
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title("New Place"));
+        Toast.makeText(this, "Click on save", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -96,8 +126,52 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.save){
             // to MainActivity
+            upload();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void upload() {
+
+        Place place=Place.getInstance();
+
+        String placeName=place.getName();
+        String placeType=place.getType();
+        String placeAtmosphere=place.getAtmosphere();
+        /*
+        Bitmap placeImage=place.getImage();
+
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        placeImage.compress(Bitmap.CompressFormat.PNG,50,byteArrayOutputStream);
+        byte[] bytes=byteArrayOutputStream.toByteArray();
+
+        ParseFile parseFile=new ParseFile("image.pnp",bytes);
+ */
+        ParseObject parseObject=new ParseObject("Places");
+        //parseObject.put("image",parseFile);
+        parseObject.put("name",placeName);
+        System.out.println("name: "+placeName);
+        parseObject.put("type",placeType);
+        System.out.println("type: "+placeType);
+
+        parseObject.put("atmosphere",placeAtmosphere);
+        parseObject.put("latitude",selectedLatitudeString);
+        parseObject.put("longitude",selectedLongitudeString);
+
+
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if( e != null){
+                    Toast.makeText(MapsActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }else{
+                    Intent intent=new Intent(MapsActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 
     private void registerLauncher() {
@@ -107,12 +181,35 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onActivityResult(Boolean result) {
                 if(result){
                     //permission granted!!
-                    mMap = getmMap;
 
-                    // Add a marker in Sydney and move the camera
-                    LatLng sydney = new LatLng(48.864716, 2.349014);
-                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+
+                    locationManager = (LocationManager) MapsActivity.this.getSystemService(Context.LOCATION_SERVICE);
+                    locationListener=new LocationListener() {
+                        @Override
+                        public void onLocationChanged(@NonNull Location location) {
+                            SharedPreferences sharedPreferences=MapsActivity.this.getSharedPreferences("com.mustafaunlu.foursquareclone",MODE_PRIVATE);
+                            boolean firstTry=sharedPreferences.getBoolean("firstTime",true);
+
+                            if(firstTry){
+                                LatLng userLocation=new LatLng(location.getLatitude(),location.getLongitude());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
+                                sharedPreferences.edit().putBoolean("firstTime",false).apply();
+                            }
+                        }
+                    };
+
+                    if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION) ==PackageManager.PERMISSION_GRANTED){
+
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                        mMap.clear();
+
+                        Location lastKnownLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if(lastKnownLocation != null){
+                            LatLng lastUserLocation=new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15));
+                        }
+                    }
+
 
                 }else{
                     //permission denied
@@ -122,5 +219,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
 
     }
+
 
 }
