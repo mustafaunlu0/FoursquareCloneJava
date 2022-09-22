17f3e55620e6b74cb5bd23b291b091a9d318d9ef
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-22 17:09:14 +0300

    maps permission

--- app/src/main/java/com/mustafaunlu/foursquareclone/activity/MapsActivity.java
+++ app/src/main/java/com/mustafaunlu/foursquareclone/activity/MapsActivity.java
@@ -1,8 +1,21 @@
 package com.mustafaunlu.foursquareclone.activity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +23,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
 import com.mustafaunlu.foursquareclone.R;
 import com.mustafaunlu.foursquareclone.databinding.ActivityMapsBinding;
 
@@ -17,13 +31,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private GoogleMap getmMap;
+
+    //Launcher
+    private ActivityResultLauncher<String> permissionLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        registerLauncher();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -31,22 +50,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getmMap=googleMap;
+
+
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.getRoot(),"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                    }
+                }).show();
+            }else{
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+            }
+        }else{
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        }
+
+
+
     }
+
+    private void registerLauncher() {
+
+        permissionLauncher=this.registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    //permission granted!!
+                    mMap = getmMap;
+
+                    // Add a marker in Sydney and move the camera
+                    LatLng sydney = new LatLng(48.864716, 2.349014);
+                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+
+                }else{
+                    //permission denied
+                    Toast.makeText(MapsActivity.this, "Permission needed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
