e7098a8fa839afe0a42f705e0617a83fbc2fda46
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-24 13:51:57 +0300

    ListView is added and the connection between activities is provided.

--- app/src/main/java/com/mustafaunlu/foursquareclone/activity/MainActivity.java
+++ app/src/main/java/com/mustafaunlu/foursquareclone/activity/MainActivity.java
@@ -8,23 +8,76 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.mustafaunlu.foursquareclone.R;
 import com.mustafaunlu.foursquareclone.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ArrayList<String> placeName;
+    ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        //ArrayAdapter - ListView
+        placeName=new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,placeName);
+        binding.listView.setAdapter(arrayAdapter);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(getApplicationContext(),DetailActivity.class);
+                intent.putExtra("name",placeName.get(i));
+                startActivity(intent);
+            }
+        });
+
+        download();
+
+    }
+
+    private void download() {
+        ParseQuery<ParseObject> query=ParseQuery.getQuery("Places");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e!=null){
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }else{
+                    if(objects.size()>0){
+
+                        placeName.clear();
+                        for(ParseObject object : objects){
+                            placeName.add(object.getString("name"));
+                            arrayAdapter.notifyDataSetChanged();
+                        }
+
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
